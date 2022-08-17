c53adee8e496b10103fcfc2348a526e369051da0
Kunisugi <jav.navarrete@duocuc.cl>
2022-08-16 22:43:15 -0400

    ASegurado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clase_2_programacion';
+  edad: number = 30;
+  direccion: string = "Calle falsa 1234";
+  resultado: number=0;
+  nro_random: number= 0;
+  dias_semana: string[] = ["dia", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+  dia:string = "";
+  contador: number = 0;
+  sumar(numero1: number, numero2: number): number{
+    return numero1 + numero2;
+  }
+  potencia(numbero1: number, numbero2: number): number{
+    return numbero1 ** numbero2;
+  }
+
+  sumarDos(): void {
+    this.resultado = this.resultado + 2
+  }
+
+  numeroRandom(): void {
+    this.nro_random = Math.ceil(Math.random()*100000)
+  }
+  diasSemana(): void {
+   this.dia = this.dias_semana[Math.ceil(Math.random()*(7))]}
+
+
+
 }
